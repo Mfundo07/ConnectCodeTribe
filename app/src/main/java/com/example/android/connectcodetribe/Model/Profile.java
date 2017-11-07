3308dcec20de40e35f9b3f96678110b7fef3ce34
Mfundo07 <mfundombokoma07@gmail.com>
2017-11-07 14:42:37 +0200

    commit

---
+++ app/src/main/java/com/example/android/connectcodetribe/Model/Profile.java
@@ -0,0 +1,122 @@
+package com.example.android.connectcodetribe.Model;
+
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Admin on 11/7/2017.
+ */
+
+public class Profile {
+    private String mProfileImage;
+    private String mProfileName;
+    private String mStatus;
+    private String mOccupation;
+    private String mProfileEmail;
+    private String mCodeTribe;
+    private String mProfileSurname;
+
+    public String getProfileSurname() {
+        return mProfileSurname;
+    }
+
+    public void setProfileSurname(String profileSurname) {
+        mProfileSurname = profileSurname;
+    }
+
+    public Profile(String profileImage, String profileName, String status, String occupation, String profileEmail, String codeTribe, String profileSurname) {
+
+        mProfileImage = profileImage;
+        mProfileName = profileName;
+        mStatus = status;
+        mOccupation = occupation;
+        mProfileEmail = profileEmail;
+        mCodeTribe = codeTribe;
+        mProfileSurname = profileSurname;
+    }
+
+    public String getCodeTribe() {
+        return mCodeTribe;
+    }
+
+    public void setCodeTribe(String codeTribe) {
+        mCodeTribe = codeTribe;
+    }
+
+    public Profile(String profileImage, String profileName, String status, String occupation, String profileEmail, String codeTribe) {
+
+        mProfileImage = profileImage;
+        mProfileName = profileName;
+        mStatus = status;
+        mOccupation = occupation;
+        mProfileEmail = profileEmail;
+        mCodeTribe = codeTribe;
+    }
+
+    public String getProfileImage() {
+        return mProfileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        mProfileImage = profileImage;
+    }
+
+    public String getProfileName() {
+        return mProfileName;
+    }
+
+    public void setProfileName(String profileName) {
+        mProfileName = profileName;
+    }
+
+    public String getStatus() {
+        return mStatus;
+    }
+
+    public void setStatus(String status) {
+        mStatus = status;
+    }
+
+    public String getOccupation() {
+        return mOccupation;
+    }
+
+    public void setOccupation(String occupation) {
+        mOccupation = occupation;
+    }
+
+    public String getProfileEmail() {
+        return mProfileEmail;
+    }
+
+    public void setProfileEmail(String profileEmail) {
+        mProfileEmail = profileEmail;
+    }
+
+    public Profile(String profileImage, String profileName, String status, String occupation, String profileEmail) {
+
+        mProfileImage = profileImage;
+        mProfileName = profileName;
+        mStatus = status;
+        mOccupation = occupation;
+        mProfileEmail = profileEmail;
+    }
+
+    public Profile() {
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("display_picture", mProfileImage);
+        result.put("name", mProfileName);
+        result.put("occupation", mOccupation);
+        result.put("email", mProfileEmail);
+        result.put("status", mStatus);
+        result.put("codeTribe", mCodeTribe);
+
+        return result;
+    }
+}
