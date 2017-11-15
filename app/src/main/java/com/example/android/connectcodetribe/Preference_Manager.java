716b5e21141beb163592db545ee4b5b0c5e2c484
khorolat <thabangtshepo.khorola@gmail.com>
2017-11-15 14:50:25 +0200

    Commit

---
+++ app/src/main/java/com/example/android/connectcodetribe/Preference_Manager.java
@@ -0,0 +1,54 @@
+package com.example.android.connectcodetribe;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Admin on 11/15/2017.
+ */
+
+public class Preference_Manager {
+    private Context context;
+    private SharedPreferences sharedPreferences;
+    public Preference_Manager(Context context){
+
+        this.context = context;
+        getSharedPreference();
+
+    }
+
+    private void getSharedPreference(){
+        sharedPreferences = context.getSharedPreferences(context.getString(R.string.my_preference), Context.MODE_PRIVATE);
+    }
+
+    public void writePreference(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(context.getString(R.string.my_preference_key),"INIT_OK");
+        editor.commit();
+
+    }
+
+    public boolean checkPreference(){
+        boolean status = false;
+
+        if (sharedPreferences.getString(context.getString(R.string.my_preference_key),"null").equals("null")){
+
+            status = false;
+        }
+        else {
+            status = true;
+
+
+        }
+        return status;
+
+
+
+    }
+
+    public void clearPreferences(){
+        sharedPreferences.edit().clear().commit();
+    }
+
+
+}
