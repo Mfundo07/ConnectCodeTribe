e4c2ba6331e2a42f247a1a5730d87d507beb325d
Mfundo07 <mfundombokoma07@gmail.com>
2017-11-15 11:13:58 +0200

    commit click of list item to scroll fragment activity

--- app/src/main/java/com/example/android/connectcodetribe/ScrollingFragment.java
+++ app/src/main/java/com/example/android/connectcodetribe/ScrollingFragment.java
@@ -57,7 +57,7 @@ public class ScrollingFragment extends AppCompatActivity {
        if (getIntent().getExtras().getString("CodeTribe") != null){
             userProfileCodeTribe.setText(mCodeTribe);}
             else{
-           userProfileCodeTribe.setText(" ");
+           userProfileCodeTribe.setText("  ");
        }
 
        userProfileSurname.setText(mSurname);
