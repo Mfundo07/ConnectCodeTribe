2b5c3f0372267f748a7fbfdec469979b828d94e0
khorolat <thabangtshepo.khorola@gmail.com>
2017-11-16 09:36:46 +0200

    Commit

--- app/src/main/java/com/example/android/connectcodetribe/Fragments/TabFragment.java
+++ app/src/main/java/com/example/android/connectcodetribe/Fragments/TabFragment.java
@@ -58,7 +58,7 @@ public class TabFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_main2, container, false);
             TextView textView = (rootView) .findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
