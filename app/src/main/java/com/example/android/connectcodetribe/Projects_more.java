0b22a1befbde2d215b616082c728c8c704e3ab1c
khorolat <thabangtshepo.khorola@gmail.com>
2017-11-20 15:47:53 +0200

    Commit

--- app/src/main/java/com/example/android/connectcodetribe/Projects_more.java
+++ app/src/main/java/com/example/android/connectcodetribe/Projects_more.java
@@ -1,13 +1,28 @@
 package com.example.android.connectcodetribe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Projects_more extends AppCompatActivity {
+
+    Button bnAddProject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projects_more);
+
+        bnAddProject = (Button) findViewById(R.id.bnAddproject);
+
+        bnAddProject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Projects_more.this,ProjectsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
