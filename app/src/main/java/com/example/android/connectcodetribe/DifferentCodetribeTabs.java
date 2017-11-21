0ebe10d34e15f38855e57cceebbbd2cad7c8368f
nonclair <nonhlanhlamokoena11@gmail.com>
2017-11-21 11:15:14 +0200

    commit

--- app/src/main/java/com/example/android/connectcodetribe/DifferentCodetribeTabs.java
+++ app/src/main/java/com/example/android/connectcodetribe/DifferentCodetribeTabs.java
@@ -2,12 +2,15 @@ package com.example.android.connectcodetribe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.android.connectcodetribe.Adapters.CategoryAdapter;
 import com.example.android.connectcodetribe.profile.ProfileActivity;
@@ -20,18 +23,23 @@ public class DifferentCodetribeTabs extends AppCompatActivity {
         setContentView(R.layout.landing_activity);
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
 
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              Intent kabza= new Intent(DifferentCodetribeTabs.this,ProfileActivity.class);
+              startActivity(kabza);
+            }
+        });
+
         CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager(), this);
 
         viewPager.setAdapter(adapter);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
-
-
-
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
