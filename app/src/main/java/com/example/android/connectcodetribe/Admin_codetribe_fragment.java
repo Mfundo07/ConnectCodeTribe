2f9edab04f24be38298727a9ceb7811414253b0c
nonclair <nonhlanhlamokoena11@gmail.com>
2017-11-23 16:26:02 +0200

    commit

--- app/src/main/java/com/example/android/connectcodetribe/Admin_codetribe_fragment.java
+++ app/src/main/java/com/example/android/connectcodetribe/Admin_codetribe_fragment.java
@@ -1,8 +1,68 @@
 package com.example.android.connectcodetribe;
 
-/**
- * Created by Admin on 11/23/2017.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
-public class Admin_codetribe_fragment {
+import com.example.android.connectcodetribe.Adapters.CategoryAdapter;
+import com.example.android.connectcodetribe.profile.ProfileActivity;
+
+public class Admin_codetribe_fragment extends AppCompatActivity {
+
+    FloatingActionButton mProfileBackFabButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_codetribe_fragment);
+
+        mProfileBackFabButton = findViewById(R.id.profile_back_fab_button);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+
+
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager(), this);
+
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.sign_out_menu:
+                startActivity(new Intent(this, LoginActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
 }
