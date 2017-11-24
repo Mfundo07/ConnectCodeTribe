c6ac1573ba27a6e5d6f8baa210388b4cddc5a5f3
nonclair <nonhlanhlamokoena11@gmail.com>
2017-11-24 11:04:39 +0200

    commit

--- app/src/main/java/com/example/android/connectcodetribe/Admin_Login_Activity.java
+++ app/src/main/java/com/example/android/connectcodetribe/Admin_Login_Activity.java
@@ -1,7 +1,10 @@
 package com.example.android.connectcodetribe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Admin_Login_Activity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Admin_Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__login_);
+
+        Button button = (Button)findViewById(R.id.admin_register);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent kb =new Intent(Admin_Login_Activity.this, Admin_register.class);
+                startActivity(kb);
+            }
+        });
+
+
     }
 }
