ba0d21331e36cb7a5446956aa495a42ff64686a4
Kabelo <kabelosidney@gmail.com>
2017-12-01 11:03:03 +0200

    commit

--- app/src/main/java/com/example/android/connectcodetribe/Users_request_Activity.java
+++ app/src/main/java/com/example/android/connectcodetribe/Users_request_Activity.java
@@ -1,13 +1,23 @@
 package com.example.android.connectcodetribe;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 public class Users_request_Activity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_request_);
+
     }
 }
