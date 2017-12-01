c7228b0728a921526d971df9c11c70e24e37ec30
Kabelo <kabelosidney@gmail.com>
2017-12-01 12:08:25 +0200

    commit

--- app/src/main/java/com/example/android/connectcodetribe/Users_request_Activity.java
+++ app/src/main/java/com/example/android/connectcodetribe/Users_request_Activity.java
@@ -1,7 +1,10 @@
 package com.example.android.connectcodetribe;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,13 +14,36 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.android.connectcodetribe.Adapters.Admin_History_CategoryAdapter;
+import com.example.android.connectcodetribe.Adapters.CategoryAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 public class Users_request_Activity extends AppCompatActivity {
 
+    FirebaseUser currentUser;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_request_);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarkb);
+        setSupportActionBar(toolbar);
+
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null){
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();}
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        Admin_History_CategoryAdapter adapter = new Admin_History_CategoryAdapter(getSupportFragmentManager(), this);
+
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 }
