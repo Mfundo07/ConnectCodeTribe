af52b58c7d973673633d0002fcd8de45eade5a4c
Mfundo07 <mfundombokoma07@gmail.com>
2017-12-04 13:50:09 +0200

    commit

---
+++ app/src/main/java/com/example/android/connectcodetribe/CodeTribeSelectActivity.java
@@ -0,0 +1,91 @@
+package com.example.android.connectcodetribe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+/**
+ * Created by Admin on 12/4/2017.
+ */
+
+public class CodeTribeSelectActivity extends AppCompatActivity {
+
+    public static final int TRIBE_SOWETO = 1;
+    public static final int TRIBE_PRETORIA = 2;
+    public static final int TRIBE_TEMBISA = 3;
+
+    private int mTribe = TRIBE_SOWETO;
+
+    private boolean mUserHasChanged = false;
+
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mUserHasChanged = true;
+            return false;
+        }
+
+    };
+
+
+    private Spinner codeTribeSpinner;
+    private Button nextStepButton;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.codetribe_select);
+
+        codeTribeSpinner = findViewById(R.id.codeTribe_select_spinner);
+        nextStepButton = findViewById(R.id.nextStep_button);
+
+        codeTribeSpinner.setOnTouchListener(mTouchListener);
+        setupCodeTribeSpinner();
+
+        nextStepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+    }
+
+    private void setupCodeTribeSpinner(){
+        ArrayAdapter codeTribeSinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_code_tribe_option, android.R.layout.simple_spinner_item);
+        codeTribeSinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        codeTribeSpinner.setAdapter(codeTribeSinnerAdapter);
+        codeTribeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)){
+                    if (selection.equals(R.string.tab_soweto)){
+                        mTribe = TRIBE_SOWETO;
+                    }else if (selection.equals(R.string.tab_tembisa)){
+                        mTribe = TRIBE_TEMBISA;
+                    }else {
+                        mTribe = TRIBE_PRETORIA;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mTribe = TRIBE_SOWETO;
+            }
+        });
+
+    }
+
+}
