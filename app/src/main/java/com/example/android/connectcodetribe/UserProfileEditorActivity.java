234e554b9531ba93848c24ea9a704d254d75bde7
Mfundo07 <mfundombokoma07@gmail.com>
2017-12-07 09:28:08 +0200

    commit

--- app/src/main/java/com/example/android/connectcodetribe/UserProfileEditorActivity.java
+++ app/src/main/java/com/example/android/connectcodetribe/UserProfileEditorActivity.java
@@ -363,7 +363,8 @@ Calendar mCalendar = Calendar.getInstance();
 
                 mProfileNameEditText.setText((String) dataSnapshot.child(currentUser.getUid()).child("name").getValue());
                 mProfileSurnameEditText.setText((String) dataSnapshot.child(currentUser.getUid()).child("surname").getValue());
-                mProfileAgeEditText.setText( dataSnapshot.child(currentUser.getUid()).child("age").getValue().toString());
+                if (dataSnapshot.child(currentUser.getUid()).child("age").getValue() != null){
+                mProfileAgeEditText.setText( dataSnapshot.child(currentUser.getUid()).child("age").getValue().toString());};
                 mProfileCellPhoneNumberEditText.setText((String) dataSnapshot.child(currentUser.getUid()).child("mobileNo").getValue());
                 mProfileEmployeeCodeEditText.setText((String) dataSnapshot.child(currentUser.getUid()).child("employeeCode").getValue());
                 mProfileQualificationEditText.setText((String) dataSnapshot.child(currentUser.getUid()).child("highestQualification").getValue());
