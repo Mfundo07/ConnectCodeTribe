41b37f142aed1660398dc048edbc15300e1ddd5e
Kabelo <kabelosidney@gmail.com>
2017-12-08 10:18:07 +0200

    commit

---
+++ app/src/test/java/com/example/android/connectcodetribe/RequestActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.android.connectcodetribe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Admin on 12/7/2017.
+ */
+public class RequestActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+}
